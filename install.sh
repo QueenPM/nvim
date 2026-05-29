1ea6534fe7a29176f79733d268ce755f670f1e5b
QueenPM <danimark644@gmail.com>
2026-05-29 18:25:49 +0100

    chore: added install.sh script
    wip stuff, but really to help me quickly install utils for neovim &
    general stuff

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Installing Neovim"
+
+sudo apt-get update && sudo apt-get install -y neovim git curl
+
+echo "Installing ripgrep"
+
+sudo apt-get install ripgrep
+
+echo "Installing cargo binstall"
+
+curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+
+echo "Installing tree sitter cli"
+
+cargo binstall tree-sitter-cli
+
+echo "Install fd"
+
+apt install fd-find
+
+echo "Install 7z"
+
+sudo apt install 7zip
+
+echo "Install lazygit"
+
+sudo apt install lazygit
